56947c33289254fd5d4847e0f7588c123cb5a948
Mateus Machado <mtarcinalli@gmail.com>
2021-09-09 08:44:07 -0300

    acentuação

--- 04_filas/estatica/main.cpp
+++ 04_filas/estatica/main.cpp
@@ -1,11 +1,13 @@
 #include <iostream>
 #include <cstdlib>
+#include <locale.h>
 
 using namespace std;
 
 #include "Fila.cpp"
 
 int main() {
+    setlocale(LC_ALL, "Portuguese");
     Fila minhaFila;
     
     for (int i = 10 ; i < 20 ; i++) {
